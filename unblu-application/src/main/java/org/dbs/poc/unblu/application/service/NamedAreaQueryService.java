c26b48e71f8939628ecd44b0f0bb6a4292c14e95
Daniel Missud <daniel@missud.eu>
2026-03-16 22:20:45 +0100

    feat(service): add `searchAgentsByNamedArea` support in `NamedAreaQueryService`
    - Implement `SearchAgentsByNamedAreaUseCase` in `NamedAreaQueryService`.
    - Update `UnbluPort` with `searchAgentsByNamedArea` method for agent retrieval by named area.

--- unblu-application/src/main/java/org/dbs/poc/unblu/application/service/NamedAreaQueryService.java
+++ unblu-application/src/main/java/org/dbs/poc/unblu/application/service/NamedAreaQueryService.java
@@ -2,8 +2,10 @@ package org.dbs.poc.unblu.application.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.dbs.poc.unblu.application.port.in.SearchAgentsByNamedAreaUseCase;
 import org.dbs.poc.unblu.application.port.in.SearchNamedAreasUseCase;
 import org.dbs.poc.unblu.domain.model.NamedAreaInfo;
+import org.dbs.poc.unblu.domain.model.PersonInfo;
 import org.dbs.poc.unblu.domain.port.secondary.UnbluPort;
 import org.springframework.stereotype.Service;
 
@@ -12,7 +14,7 @@ import java.util.List;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class NamedAreaQueryService implements SearchNamedAreasUseCase {
+public class NamedAreaQueryService implements SearchNamedAreasUseCase, SearchAgentsByNamedAreaUseCase {
 
     private final UnbluPort unbluPort;
 
